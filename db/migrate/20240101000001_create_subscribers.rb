9636e7e4c51e695acc10a4cd21bcc71b50d1d4a3
Neeharika K <neeharika019@gmail.com>
2025-07-04 00:21:30 -0500

    Fix migration to use Rails 6.1 syntax

--- db/migrate/20240101000001_create_subscribers.rb
+++ db/migrate/20240101000001_create_subscribers.rb
@@ -1,4 +1,4 @@
-class CreateSubscribers < ActiveRecord::Migration[7.0]
+class CreateSubscribers < ActiveRecord::Migration[6.1]
   def change
     create_table :subscribers do |t|
       t.string :name
